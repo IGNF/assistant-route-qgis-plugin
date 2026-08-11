7774fa085098a4f5dc164d2857330ac1ac3212c6
Gerome Pecheur <gerome.pecheur@ign.fr>
2026-08-12 01:47:03 +0200

    modif pour passer la validation bandit

--- fonction.py
+++ fonction.py
@@ -2,43 +2,12 @@ import os.path
 from qgis.PyQt.QtWidgets import QMessageBox
 from qgis.PyQt.QtCore import Qt
 
-import subprocess
 import webbrowser
 from .constante import *
-
-def ecrirelog(text):
-    ficlog = open(os.path.dirname(__file__) + "/log.txt", "a")
-    ficlog.write(text)
-    ficlog.close()
-    print("log !!")
-
-def ecrire_debut_fichier(text):
-    fic = os.path.dirname(__file__) + "/log.txt"
-
-    text_origine = ""
-    if os.path.isfile(fic):
-        with open(os.path.dirname(__file__) + "/log.txt", 'r', encoding='utf-8') as file:
-            text_origine = file.read()
-        with open(os.path.dirname(__file__) + "/log.txt", 'w', encoding='utf-8') as file:
-            file.write(text + text_origine)
-    else:
-        with open(os.path.dirname(__file__) + "/log.txt", 'w', encoding='utf-8') as file:
-            file.write(text + text_origine)
 
 def afficheDoc():
     webbrowser.open("https://ignf.github.io/assistant-route-qgis-plugin/")
 
-def afficherlog():
-    fic = os.path.dirname(__file__) + "/transaction.xlsx"
-
-    if not os.path.isfile(fic):
-        afficheerreur("Le fichier de log n'existe pas\nIl sera crée dès la premiére transaction", "Information")
-    else:
-        subprocess.Popen(["start", "excel", fic], shell=True)
-
-def affiches_spec_bdtopo():
-    import webbrowser
-    webbrowser.open("https://bdtopoexplorer.ign.fr/")
 
 def afficheerreur(text, titre=TITRE):
     msg = QMessageBox()
